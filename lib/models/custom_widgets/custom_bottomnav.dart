c279fe5fe608b93ccd222ed7e01f4ed018bcefd4
Krishna <srinivaskrishnask2002@gmail.com>
2023-03-29 15:27:43 +0530

    update

--- lib/models/custom_widgets/custom_bottomnav.dart
+++ lib/models/custom_widgets/custom_bottomnav.dart
@@ -9,88 +9,92 @@ class CustomBottomNav extends StatelessWidget {
     double screenHeight = MediaQuery.of(context).size.height;
     double screenWidth = MediaQuery.of(context).size.width;
     return SizedBox(
-          height: screenHeight/12,
-          child: Padding(
-            padding: const EdgeInsets.only(top: 10, bottom: 5),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              children: <Widget>[
-                InkWell(
-                  onTap: () {},
-                  child: Column(
-                    children: <Widget>[
-                      Image.asset(
-                        'assets/logos/home.png',
-                        height: 26,
-                        width: 26,
-                      ),
-                      const SizedBox(height: 6),
-                      const Text(
-                        'Home',
-                        textAlign: TextAlign.center,
-                        style: TextStyle(
-                          color: Color_Red,
-                        ),
-                      ),
-                    ],
+      height: screenHeight / 12,
+      child: Padding(
+        padding: const EdgeInsets.only(top: 10, bottom: 5),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: <Widget>[
+            InkWell(
+              onTap: () {},
+              child: Column(
+                children: <Widget>[
+                  Image.asset(
+                    'assets/logos/home.png',
+                    height: 26,
+                    width: 26,
                   ),
-                ),
-                InkWell(
-                  onTap: () {},
-                  child: Column(
-                    children: <Widget>[
-                      Row(
-                        children: const <Widget>[
-                          Icon(
-                            Icons.star_rate_rounded,
-                            color: Color_Black,
-                            size: 11,
-                          ),
-                          SizedBox(width: 8),
-                        ],
-                      ),
-                      const Text(
-                        'LIVE',
-                        textAlign: TextAlign.center,
-                        style: TextStyle(
-                          color: Color_Black,
-                          fontWeight: FontWeight.bold,
-                          letterSpacing: -0.5,
-                          fontSize: 14,
-                        ),
-                      ),
-                      const SizedBox(height: 5.5),
-                      const Text(
-                        'Events',
-                        style: TextStyle(
-                          color: Colors.grey,
-                        ),
-                      ),
-                    ],
+                  const SizedBox(height: 6),
+                  const Text(
+                    'Home',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      color: Color_Red,
+                    ),
                   ),
-                ),
-                InkWell(
-                  onTap: () {},
-                  child: Column(
+                ],
+              ),
+            ),
+            InkWell(
+              onTap: () {},
+              child: Column(
+                children: <Widget>[
+                  Row(
                     children: const <Widget>[
                       Icon(
-                        Icons.person,
+                        Icons.star_rate_rounded,
                         color: Color_Black,
-                        size: 28,
+                        size: 11,
                       ),
-                      SizedBox(height: 5.5),
-                      Text(
-                        'Profile',
-                        style: TextStyle(
-                          color: Colors.grey,
-                        ),
-                      ),
+                      SizedBox(width: 8),
                     ],
                   ),
-                ),
-              ],
+                  const Text(
+                    'LIVE',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      color: Color_Black,
+                      fontWeight: FontWeight.bold,
+                      letterSpacing: -0.5,
+                      fontSize: 14,
+                    ),
+                  ),
+                  const SizedBox(height: 5.5),
+                  const Text(
+                    'Events',
+                    style: TextStyle(
+                      color: Colors.grey,
+                    ),
+                  ),
+                ],
+              ),
             ),
-          ),
-        );
+            Container(
+              height: screenHeight,
+              width: screenWidth/3,
+              child: InkWell(
+                onTap: () {},
+                child: Column(
+                  children: const <Widget>[
+                    Icon(
+                      Icons.person,
+                      color: Color_Black,
+                      size: 28,
+                    ),
+                    SizedBox(height: 5.5),
+                    Text(
+                      'Profile',
+                      style: TextStyle(
+                        color: Colors.grey,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
